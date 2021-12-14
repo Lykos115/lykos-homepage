52742fd1b10d9bbeb58a87aaca455e4cd9fa35a3
Francisco Herrera <fherrera-guerrero@csumb.edu>
2021-12-13 20:41:37 -0800

    footer for the site

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <div className='text-center opacity-40 text-sm'>
+    &copy; {new Date().getFullYear()} Francisco Herrera. All Rights Reserved
+  </div>
+)
+
+export default Footer
