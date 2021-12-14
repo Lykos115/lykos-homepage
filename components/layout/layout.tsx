a287b72eb2707db8d9afe19310d88f3bb738c99d
Francisco Herrera <fherrera-guerrero@csumb.edu>
2021-12-13 20:38:46 -0800

    template for layout

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Footer from '../footer'
 
 interface LayoutProps {
   children: React.ReactNode
@@ -6,18 +7,19 @@ interface LayoutProps {
 
 const Main = ({children} :LayoutProps) => {
   return(
-    <div className='w-screen bg-[#362222]'>
+    <div className='w-auto h-auto bg-[#2b2b2b] text-white pb-8 px-4'>
       <Head>
   
       </Head>
       
-      <div className='container max-w-md mx-auto'>
+      <div className='md:w-[320px] md:h-[320px] w-auto h-auto mx-auto'> place holder logo</div>
+      <div className='container max-w-md mx-auto pt-14'>
         {children}
         
+        <Footer />
       </div>
-
     </div>
-    )
+  )
 }
 
 export default Main
