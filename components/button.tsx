cc793984a8cf70c17206cac692211f20b31f749c
Francisco Herrera <fherrera-guerrero@csumb.edu>
2021-12-13 20:41:05 -0800

    button for portfolio

---
+++ components/button.tsx
@@ -0,0 +1,15 @@
+
+const button = () => (
+  <div className='my-4 box-border text-center'>
+    <button className='px-4 min-w-[1rem] inline-flex appearance-none items-center justify-center select-none relative whitespace-nowrap align-middle outline-offset-2 w-auto rounded-md font-semibold h-10 text-base bg-teal-200 text-gray-800'>
+      My Portfolio
+      <span className='inline-flex self-center shrink-0 ml-1'>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+        </svg>
+      </span>
+    </button>
+  </div>
+)
+
+export default button
